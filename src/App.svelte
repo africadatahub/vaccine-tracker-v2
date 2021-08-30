1a7488955217f2e13aef748b6ec44497848b6333
Alastair Otter <alastair@mediahack.co.za>
2021-08-30 17:36:41 +0200

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,21 @@
+<script>
+  // import logo from './assets/svelte.png'
+  // import Counter from './lib/Counter.svelte'
+  import NavBar from './components/NavBar.svelte'
+  import SmallCharts from './components/SmallCharts.svelte'
+  import MainDisplay from './components/MainDisplay.svelte'
+</script>
+
+<main>
+  <div id="container">
+    <header>
+      <h1>ADH Vaccine Tracker</h1>
+    </header>
+    <NavBar />
+    <SmallCharts />
+    <MainDisplay />
+  </div>
+</main>
+
+<style>
+</style>
---
+++ src/components/SmallCharts.svelte
@@ -0,0 +1,20 @@
+<section id="small-charts">
+  <div class="chart">Chart</div>
+  <div class="chart">Chart</div>
+  <div class="chart">Chart</div>
+  <div class="chart">Chart</div>
+</section>
+
+<style>
+  #small-charts {
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    grid-gap: 20px;
+    margin-top: 30px;
+  }
+  .chart {
+    padding: 20px;
+    background: #fff;
+    border-radius: 6px;
+  }
+</style>
