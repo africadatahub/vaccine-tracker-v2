a40a4be2a7fcfba14535f0d3fffddef5dd62a913
Nzuzo_Malinga <nzuzomal@gmail.com>
2022-12-15 13:23:39 +0200

    Media hack fix

--- app.js
+++ app.js
@@ -5,13 +5,11 @@ import { africa } from './data/africajson';
 import { countryCodes } from './data/country-codes';
 import Gradient from 'javascript-color-gradient';
 import _ from 'lodash';
-import countries from "./mediahack/countries-json.json"
+//import countries from "./mediahack/countries-json.json"
+import newVacTypes from "./mediahack/newVaccinationTypes.json"
 import africanOverview from "./mediahack/africa-overview-json.json"
 import vaccineSources from "./mediahack/vaccination-sources.json"
-import vaccinationsByCountry from "./mediahack/vaccinations-by-country-json.json"
 import vaccinationAllCountries from "./mediahack/vaccinations-all-countries-new-json.json"
-
-
 
 let parseTime = d3.timeParse('%Y-%m-%d')
 let formatDate = d3.timeFormat('%e %B, %Y')
@@ -52,7 +50,7 @@ const vm = new Vue({
       currentOverview: [],
       currentFlag: 'africa.svg',
       countryDropdown: false,
-      countries: [],
+      countries_array: [],
       countriesUrl:
         'https://api.mediahack.co.za/vaccine-tracker2-dev/countries.php',
       africaOverview: [],
@@ -70,18 +68,27 @@ const vm = new Vue({
 
     getMaxVaccineTypes() {
       this.maxVaccines = 0
+    
       this.vaccines.forEach((d) => {
+       // console.log(this.currentVaccinesReceived[d],this.currentVaccinesReceived[d],"condition")
         if (+this.currentVaccinesReceived[d] > this.maxVaccines)
           this.maxVaccines = +this.currentVaccinesReceived[d]
-      })
+      
+       })
+       console.log(this.maxVaccines,"max vaccines types")     
     },
 
     getMaxVaccineSources() {
       this.maxSources = 0
+
+     // console.log(this.currentVaccinesBought, "vaccine sources")
+
       this.sources.forEach((d) => {
         if (+this.currentVaccinesBought[d] > this.maxSources)
           this.maxSources = +this.currentVaccinesBought[d]
+        //this.maxSources = 0
       })
+      console.log(this.maxSources, "maxSources")
     },
 
     resetSelection() {
@@ -97,13 +104,17 @@ const vm = new Vue({
 
     async getCurrentHover(iso){
       await fetch(
-        `https://api.mediahack.co.za/adh/vaccine-tracker/vaccinations-by-country.php?cc=${iso}`
+      //  `https://api.mediahack.co.za/adh/vaccine-tracker/vaccinations-by-country.php?cc=${iso}`
+       `https://ckandev.africadatahub.org/api/3/action/datastore_search_sql?sql=SELECT%20*%20from%20%22489b0e29-762c-4379-b57f-5f8463178714%22%20WHERE%20iso_code%20LIKE%20%27${iso}%27`
       )
         .then((data) => 
-        //data.json()
-        vaccinationsByCountry
+
+          data.json()  
         )
-        .then((data) => {
+        .then((result) => {
+   
+          let data = result.result.records   
+
           data[data.length - 1].total_vaccinations =
             data[data.length - 1].total_vaccine_doses_to_date
           data[data.length - 1].total_vaccinations_per_hundred = (
@@ -113,23 +124,27 @@ const vm = new Vue({
           ).toFixed(2)
           data[data.length - 1].total_vaccinations = +data[data.length - 1].total_vaccinations
           this.currentHover= data[data.length - 1]
-          
-        }).catch(()=>{
-          console.log("replace")
+          console.log(this.currentHover, "hover data")
+        }).catch((err)=>{
+          console.log(err)
         })
     },
 
     showCountry(iso) {
+
       this.currentVaccinesBought = this.vaccinesBought.filter(
         (d) => d.iso_code === iso
       )[0]
       this.currentVaccinesReceived = this.vaccinesReceived.filter(
+       
         (d) => d.iso_code === iso
       )[0]
+        
+      //console.log(this.currentVaccinesBought,this.currentVaccinesReceived, "data for selecting country")
 
       this.getMaxVaccineTypes()
       this.getMaxVaccineSources()
-
+  
       this.currentVaccinesBought.covax = +this.currentVaccinesBought.covax
       this.currentVaccinesBought.bought = +this.currentVaccinesBought.bought
       this.currentVaccinesBought.borrowed = +this.currentVaccinesBought.borrowed
@@ -138,12 +153,19 @@ const vm = new Vue({
         +this.currentVaccinesBought.grand_total
 
       this.currentFlag = this.convertCode(iso)[0].iso_2.toLowerCase() + '.svg'
-
+      
       let overview = fetch(
-        `https://api.mediahack.co.za/adh/vaccine-tracker/vaccinations-by-country.php?cc=${iso}`
+    
+       // `https://api.mediahack.co.za/adh/vaccine-tracker/vaccinations-by-country.php?cc=${iso}`
+       `https://ckandev.africadatahub.org/api/3/action/datastore_search_sql?sql=SELECT%20*%20from%20%22489b0e29-762c-4379-b57f-5f8463178714%22%20WHERE%20iso_code%20LIKE%20%27${iso}%27`
       )
-        .then((data) => data.json())
+        .then((data) => 
+          data.json()
+        )
         .then((data) => {
+
+          console.log(data,"current country")
+
           this.currentCountry = data[0].country
           data[data.length - 1].total_vaccinations =
             data[data.length - 1].total_vaccine_doses_to_date
@@ -152,9 +174,10 @@ const vm = new Vue({
               +data[0].population) *
             100
           ).toFixed(2)
+
           this.currentOverview = data[data.length - 1]
         }).catch(()=>{
-          console.warn("replace with vaccinations by country data with iso parameter")
+          
         })
     },
 
@@ -166,8 +189,10 @@ const vm = new Vue({
         .then((response) =>
          //response.json()
          vaccineSources
+
          )
         .then((response) => {
+          console.log(vaccineSources)
           response.forEach((d) => {
             
             d.covax = +d.covax
@@ -179,34 +204,37 @@ const vm = new Vue({
           this.vaccinesBought = response
           this.updateAfricaTypes()
         }).catch(()=>{
-          console.log("replace")
+     
         })
     },
     
-    // async getVaccinesReceived() {
-    //   await fetch(
-    //     'https://api.mediahack.co.za/adh/vaccine-tracker/vaccinations-types.php'
-    //   )
-    //     .then((response) => response.json())
-    //     .then((response) => {
-    //       response.forEach(i=>{
-    //         i.Covaxin = +i.Covaxin
-    //         i.Johnson_and_Johnson = +i.Johnson_and_Johnson
-    //         i.Moderna = +i.Moderna
-    //         i.Oxford_AstraZeneca = +i.Oxford_AstraZeneca
-    //         i.Pfizer_BioNTech = +i.Pfizer_BioNTech
-    //         i.Sinopharm = +i.Sinopharm
-    //         i.Sinovac = +i.Sinovac
-    //         i.Sputnik_V = +i.Sputnik_V
-    //       }).catch(()=>{
-    //         console.log("replace")
-    //       })
+    async getVaccinesReceived() {
+      await fetch(
+      //  'https://api.mediahack.co.za/adh/vaccine-tracker/vaccinations-types.php'
+        "https://ckandev.africadatahub.org/api/3/action/datastore_search?resource_id=624f4d24-638a-4b48-a949-b005fe0bd9ec"
+      )
+        .then((response) => response.json())
+        .then((response) => {
 
-    //       this.vaccinesReceived = response.filter((d) => d.country !== '')
+          let newArr = [...newVacTypes]
+
+          newArr.forEach(i=>{
+
+            i.Covaxin = +i.Covaxin
+            i.Johnson_and_Johnson = +i.Johnson_and_Johnson
+            i.Moderna = +i.Moderna
+            i.Oxford_AstraZeneca = +i.Oxford_AstraZeneca
+            i.Pfizer_BioNTech = +i.Pfizer_BioNTech
+            i.Sinopharm = +i.Sinopharm
+            i.Sinovac = +i.Sinovac
+            i.Sputnik_V = +i.Sputnik_V
+          })
+
+          this.vaccinesReceived = newArr.filter((d) => d.country !== '')
           
-    //       this.updateAfrica()
-    //     })
-    // },
+          this.updateAfrica()
+        })
+    },
 
     async getAfricaOverview() {
       await fetch(
@@ -224,21 +252,29 @@ const vm = new Vue({
           this.currentOverview = data[0]
           this.africaOverviewTypes = data[0]
         }).catch(()=>{
-          console.log("replace")
+          console.warn("african overview request error")
         })
 
         this.updateAfricaTypes()
     },
 
     async getCountries() {
-      await fetch(this.countriesUrl)
+      await fetch(
+        //this.countriesUrl
+        "https://ckandev.africadatahub.org/api/3/action/datastore_search?resource_id=9b82b5e7-5f30-4055-9875-09fb8d09d1d9&limit=1000"
+        )
         .then((data) => 
-        //data.json()
-        countries
+        data.json()
+        
         )
         .then((data) => {
-          this.countries = data.filter((d) => d.location !== 'Saint Helena')
+          let result = data.result.records
+
+          this.countries_array = result.filter((d) => d.countries !== 'Saint Helena')
+
+          console.log(this.countries_array, "after filtering")
         }).catch(()=>{
+
           console.warn("failed to load countries: line 241")
         })
     },
@@ -251,8 +287,9 @@ const vm = new Vue({
       vaccinationAllCountries
       )
       .then((data) => {
+
         this.mapData = data;
-        
+
         let map = L.map('map', {
           zoomControl: false,
           scrollWheelZoom: false,
@@ -264,12 +301,12 @@ const vm = new Vue({
           onEachFeature: this.onEachFeature,
           style: this.mapStyle,
         }).addTo(map)
+
         map.fitBounds(africaMap.getBounds());
 
         let buckets = '<div class="map-legend-bucket" style="background: ' + this.absScale.getColor(1) + '"></div><div class="map-legend-bucket" style="background: ' + this.absScale.getColor(25) + '"></div><div class="map-legend-bucket" style="background: ' + this.absScale.getColor(50) + '"></div><div class="map-legend-bucket" style="background: ' + this.absScale.getColor(75) + '"></div><div class="map-legend-bucket" style="background: ' + this.absScale.getColor(100) + '"></div>';
 
         document.querySelector('.map-legend-buckets').insertAdjacentHTML('beforeend', buckets);
-
 
       }).catch(()=>{
         console.log("replace")
@@ -278,6 +315,7 @@ const vm = new Vue({
     },
 
     hover(feature) {
+
       let iso = feature.target.feature.properties.ADM0_A3;
 
       if(iso != 'SAH' && iso != 'ERI' && iso != 'SOL') {
@@ -350,9 +388,9 @@ const vm = new Vue({
       let countryData = _.find(this.mapData, (d) => { return d.iso_code == feature.properties.ADM0_A3 });
 
       if(countryData != undefined) {
+
         fillColor = this.absScale.getColor(countryData.per_100);
       }
-      
 
       return {
         color: '#eee',
@@ -364,8 +402,6 @@ const vm = new Vue({
         className: feature.properties.ADM0_A3 + ' country',
       }
     },
-
-
 
     updateAfrica() {
       let vaccines = [
@@ -387,6 +423,7 @@ const vm = new Vue({
 
           }
         })
+
         this.africaOverview[v] = count
         this.africaOverviewSource = this.africaOverview
 
@@ -437,7 +474,7 @@ const vm = new Vue({
     Promise.all([
       this.getAfricaOverview(),
       this.getVaccinesBought(),
-     // this.getVaccinesReceived(),
+      this.getVaccinesReceived(),
       this.getCountries(),
       this.absScale.setGradient('#FFECEC','#329BC2').setMidpoint(100),
       this.addAfricaMap()
